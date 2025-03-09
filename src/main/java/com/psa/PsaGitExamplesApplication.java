40f74d939c6c5dbb50d6bb542ee63608ef8196e6
Anamol-Ratan <157670704+AnmolRatan7@users.noreply.github.com>
2025-03-09 22:34:17 +0530

    Update PsaGitExamplesApplication.java

--- src/main/java/com/psa/PsaGitExamplesApplication.java
+++ src/main/java/com/psa/PsaGitExamplesApplication.java
@@ -8,6 +8,7 @@ public class PsaGitExamplesApplication {
 
 	public static void main(String[] args) {
 		int x = 100;
+		int y = 200;
 		SpringApplication.run(PsaGitExamplesApplication.class, args);
 	}
 
